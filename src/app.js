2f4fa8b76df8d6e067e06cea214a3dc19e38f126
Subrata Roy <113381826+Subrata41@users.noreply.github.com>
2023-04-21 19:56:45 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -8,7 +8,29 @@ const products = JSON.parse(fs.readFileSync(`${__dirname}/data/products.json`));
 // Middlewares
 app.use(express.json());
 
-// Write GET endpoint for sending product to the client here
-// Endpoint - /api/v1/products/:name/:price
+// Endpoint for searching products by name and price
+app.get("/products/:name/:price", (req, res) => {
+  const { name, price } = req.params;
+
+  // Find the product with matching name and price
+  const product = products.find((p) => p.name === name && p.price === Number(price));
+
+  if (product) {
+    // Return the product as a JSON response
+    res.status(200).json({
+      status: "success",
+      message: "Product fetched successfully",
+      data: {
+        product,
+      },
+    });
+  } else {
+    // Return a 404 error with a message if no matching product is found
+    res.status(404).json({
+      status: "failed",
+      message: "Product not found!",
+    });
+  }
+});
 
 module.exports = app;
